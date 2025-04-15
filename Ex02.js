31a1d33356bc071ae9c89545d8e8d629bd3a7f5e
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-04-15 15:18:25 -0300

    Subindo

---
+++ Ex02.js
@@ -0,0 +1,13 @@
+// Peça uma frase e crie uma função que retorna a maior palavra da frase usando
+// split() e reduce().
+
+var prompt = require('prompt-sync')();
+
+let Frase = prompt("Qual a frase: ");
+
+
+
+Frase.reduce(function() {
+    
+    return novoAcumulador;
+  }, valorInicial);
